4a14b8993a02947fcde6ea6fb88ef98cf84adee1
Riyactruh123 <riya.reddy@ctruh.com>
2026-02-04 16:59:25 +0530

    fix

--- talking-avatar-with-ai/apps/backend/server.js
+++ talking-avatar-with-ai/apps/backend/server.js
@@ -12,9 +12,9 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-app.use(cors());
-app.use(express.json({ limit: "50mb" }));
 
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
 const processToAvatar = async (text) => {
   const completion = await groq.chat.completions.create({
     messages: [
